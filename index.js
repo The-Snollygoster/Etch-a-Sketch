cebdbe89472d9cd7bae604308b70968df35c31bb
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-23 19:14:13 +0000

    Create grid function with row
    I've created the function that will make my grid
    and I've added the loop that creates a row and
    adds 16 cells to it. I've given it some basic
    styling.
    
    I've also added a header to the HTML and some
    basic css to the container div.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 let container = document.querySelector('.container');
 
-let div = document.createElement('div');
+function createGrid() {
 
-container.appendChild(innerHTML = '<div></div>');
+    let row = document.createElement('div');
+    // not sure if this is how to make this flex
+    row.style.display = 'flex';
 
+    for (i=0; i<16; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'cell');
+        div.style.cssText = 'border: 0; margin: 0; width: 30px; height: 30px; outline: 1px solid black;';
+        row.appendChild(div);
+    }
+    container.appendChild(row);
+}
+
+createGrid()
+
+// function that creates a div called 'row'. And a loop 
+// that puts 16 square divs inside that row div.
+// 
+// That loop is nested in another loop that creates 16
+// columns, each of which are one of those rows.
