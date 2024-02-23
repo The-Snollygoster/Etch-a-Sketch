e2b2a30e6465cb2615a5c92c1f701aab1ad3ab9c
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-23 23:05:39 +0000

    Complete grid function
    I've now finished the function that creates the
    grid and it fills the predetermined container
    size.
    
    Next will be altering it so it takes the loop size
    equal to the prompt input from the button.

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 let container = document.querySelector('.container');
+let gridSize = document.querySelector('#gridSize');
 
 function createGrid() {
-
     for (r=0; r<16; r++) {
         let row = document.createElement('div');
-        row.style.display = 'flex';
+        row.style.cssText = 'display: flex; flex: 1 1 0;';
         for (i=0; i<16; i++) {
             let div = document.createElement('div');
             div.setAttribute('class', 'cell');
-            div.style.cssText = 'border: 0; margin: 0; width: 30px; height: 30px; outline: 1px solid black;';
+            div.style.cssText = 'flex: 1 1 0; border: 0; margin: 0; outline: 1px solid black;';
             row.appendChild(div);
         }
         container.appendChild(row);
     }
 }
-
 createGrid()
+
+let cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', () => {
+        cell.style.background = 'black';
+    });
+});
+
+gridSize.addEventListener('click', () => {
+    prompt('How big would you like the Grid to be?');
+})
