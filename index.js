2738531057ed8a4764f8775a32b08d45660bc378
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-23 19:30:33 +0000

    Loop row creation
    I've nested the row creator loop inside another
    loop to create the multiple rows. I've also
    changed the container to be flex-column so it all
    stacks. The grid is small but it's functioning.

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ let container = document.querySelector('.container');
 
 function createGrid() {
 
-    let row = document.createElement('div');
-    // not sure if this is how to make this flex
-    row.style.display = 'flex';
-
-    for (i=0; i<16; i++) {
-        let div = document.createElement('div');
-        div.setAttribute('class', 'cell');
-        div.style.cssText = 'border: 0; margin: 0; width: 30px; height: 30px; outline: 1px solid black;';
-        row.appendChild(div);
+    for (r=0; r<16; r++) {
+        let row = document.createElement('div');
+        row.style.display = 'flex';
+        for (i=0; i<16; i++) {
+            let div = document.createElement('div');
+            div.setAttribute('class', 'cell');
+            div.style.cssText = 'border: 0; margin: 0; width: 30px; height: 30px; outline: 1px solid black;';
+            row.appendChild(div);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
 createGrid()
-
-// function that creates a div called 'row'. And a loop 
-// that puts 16 square divs inside that row div.
-// 
-// That loop is nested in another loop that creates 16
-// columns, each of which are one of those rows.
