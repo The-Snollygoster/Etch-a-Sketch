e22aa6c25849fb2eb77c1809e7da8547d2c4b568
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-24 15:38:39 +0000

    Finish newGrid button
    I've fully built the system for creating a new
    grid of a custom size. The button works and will
    delete the old grid, and through the new listener
    with a prompt you can pass a size into the old
    function which I have changed to accept a specific
    size. When I originally call the function I just
    added a size to be passed.
    
    I've also added an outline to the container and
    deleted the cell outlines which I think looks
    cleaner.

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-let container = document.querySelector('.container');
+let container = document.querySelector('#container');
 let gridSize = document.querySelector('#gridSize');
 
-function createGrid() {
-    for (r=0; r<16; r++) {
+function createGrid(size) {
+    for (r=0; r<size; r++) {
         let row = document.createElement('div');
+        row.setAttribute('class', 'row');
         row.style.cssText = 'display: flex; flex: 1 1 0;';
-        for (i=0; i<16; i++) {
+        for (i=0; i<size; i++) {
             let div = document.createElement('div');
             div.setAttribute('class', 'cell');
-            div.style.cssText = 'flex: 1 1 0; border: 0; margin: 0; outline: 1px solid black;';
+            div.style.cssText = 'flex: 1 1 0; border: 0; margin: 0;';
             row.appendChild(div);
         }
         container.appendChild(row);
     }
-}
-createGrid()
 
-let cells = document.querySelectorAll('.cell');
+    let cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
     cell.addEventListener('mouseenter', () => {
         cell.style.background = 'black';
     });
 });
+};
+createGrid(16);
 
 gridSize.addEventListener('click', () => {
-    prompt('How big would you like the Grid to be?');
-})
+    document.getElementById("container").innerHTML = "";
+    let custom = prompt('Please pick a grid size up to 100', 16);
+    if (custom == NaN) {
+        custom = prompt('Please enter a number up to 100');
+    } else if (custom > 100) {
+        custom = prompt('The maximum grid size is 100x100. Please enter a lower value.');
+    }
+    createGrid(custom)
+});
